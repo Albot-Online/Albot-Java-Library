8daa31ae2d6163299caac69e041ad26fd54ac479
Arrayen <joeyeyey1995@gmail.com>
2018-07-01 01:08:42 +0200

    Initial commit

---
+++ src/main/java/AlbotConnection.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class AlbotConnection {
+
+    private BufferedReader in;
+    private PrintWriter out;
+    private final int bufferSize = 1024;
+
+    private boolean gameOver = false;
+    private int winner;
+
+    public AlbotConnection(String ip, int port) {
+        initConnection(ip, port);
+    }
+
+    public AlbotConnection() {
+        initConnection("127.0.0.1", 4000);
+    }
+
+    private void initConnection(String ip, int port) {
+        try {
+            Socket s = new Socket(ip, port);
+            s.setReceiveBufferSize(bufferSize);
+            in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            out = new PrintWriter(s.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String receiveState() {
+        String incomingData = null;
+        try {
+            do {
+                incomingData = in.readLine();
+            } while(incomingData == null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        handleGameOverCheck(incomingData);
+        return incomingData;
+    }
+
+    public void sendCommand(String command) {
+        out.write(command);
+        out.flush();
+    }
+
+    public String sendCommandReceiveState(String command) {
+        sendCommand(command);
+        return receiveState();
+    }
+
+    public void restartGame() {
+        System.out.println("Restarting game...");
+        sendCommand(Constants.Actions.restartGame);
+    }
+
+    public boolean gameOver() {
+        return gameOver;
+    }
+
+    public int getWinner() {
+        return winner;
+    }
+
+    private void handleGameOverCheck(String incomingData) {
+        incomingData = incomingData.trim();
+        if (incomingData.length() >= 8 && incomingData.substring(0, 8) == Constants.Fields.gameOver) {
+            if (incomingData.endsWith("-1")) {
+                winner = -1;
+                System.out.println("You lost!");
+            } else if (incomingData.endsWith("1")) {
+                winner = 1;
+                System.out.println("You won!");
+            } else if (incomingData.endsWith("0")) {
+                winner = 0;
+                System.out.println("Draw!");
+            } else
+                System.out.println("Game Over!");
+
+            gameOver = true;
+        }
+    }
+
+    public void killConnection() {
+        out.close();
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/Constants.java
@@ -0,0 +1,26 @@
+
+public final class Constants {
+
+    public static class Fields {
+        public static final String board = "board";
+        public static final String evaluate = "Evaluate";
+        public static final String possibleMoves = "PossMoves";
+        public static final String move = "Move";
+        public static final String player = "Player";
+        public static final String action = "Action";
+        public static final String winner = "Winner";
+        public static final String gameOver = "GameOver";//\n";
+        public static final String boardState = "boardState";
+    }
+
+    public static class Actions {
+        public static final String restartGame = "RestartGame";
+        public static final String makeMove = "MakeMove";
+        public static final String simMove = "SimulateMove";
+        public static final String evalBoard = "EvaluateBoard";
+        public static final String getPossMoves = "GetPossibleMoves";
+    }
+
+    public enum BoardState { PlayerWon, EnemyWon, Draw, Ongoing }
+
+}
